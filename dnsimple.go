b31646959e5f395c97a228b34b674c47b2f7ba34
Scott Barron <scott@elitists.net>
2013-09-18 09:25:47 -0400

    Factor some http client stuff into DNSimpleClient

--- dnsimple.go
+++ dnsimple.go
@@ -22,10 +22,11 @@ type Domain struct {
 
 type DNSimpleClient struct {
 	DomainToken string
+	HttpClient  *http.Client
 }
 
 func NewClient(domainToken string) *DNSimpleClient {
-	return &DNSimpleClient{DomainToken: domainToken}
+	return &DNSimpleClient{DomainToken: domainToken, HttpClient: &http.Client{}}
 }
 
 func (client *DNSimpleClient) makeRequest(method, url string, body io.Reader) (*http.Request, error) {
@@ -40,30 +41,37 @@ func (client *DNSimpleClient) makeRequest(method, url string, body io.Reader) (*
 	return req, nil
 }
 
-func (client *DNSimpleClient) FindDomain(domain, name string) (Record, error) {
-	httpClient := &http.Client{}
+func (client *DNSimpleClient) sendRequest(method, url string, body io.Reader) (string, error) {
+	req, err := client.makeRequest(method, url, body)
+	if err != nil {
+		return "", err
+	}
 
+	resp, err := client.HttpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(responseBody), nil
+}
+
+func (client *DNSimpleClient) FindDomain(domain, name string) (Record, error) {
 	reqStr := fmt.Sprintf("https://dnsimple.com/domains/%s/records?name=%s", domain, name)
 
-	req, err := client.makeRequest("GET", reqStr, nil)
-	if err != nil {
-		return Record{}, err
-	}
-
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return Record{}, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := client.sendRequest("GET", reqStr, nil)
 	if err != nil {
 		return Record{}, err
 	}
 
 	var domains []Domain
 
-	if err = json.Unmarshal(body, &domains); err != nil {
+	if err = json.Unmarshal([]byte(body), &domains); err != nil {
 		return Record{}, err
 	}
 
@@ -71,24 +79,14 @@ func (client *DNSimpleClient) FindDomain(domain, name string) (Record, error) {
 }
 
 func (record *Record) UpdateIP(client *DNSimpleClient, IP string) error {
-	httpClient := &http.Client{}
-
 	// lame, but easy enough for now
 	jsonPayload := fmt.Sprintf(`{"record": {"content": "%s"}}`, IP)
 	url := fmt.Sprintf("https://dnsimple.com/domains/%d/records/%d", record.DomainId, record.Id)
 
-	req, err := client.makeRequest("PUT", url, strings.NewReader(jsonPayload))
-	if err != nil {
-		return err
-	}
-
-	resp, err := httpClient.Do(req)
-	defer resp.Body.Close()
-
+	_, err := client.sendRequest("PUT", url, strings.NewReader(jsonPayload))
 	if err != nil {
 		return err
 	}
 
 	return nil
-
 }
