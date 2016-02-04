1711ebea43a2d0744b0b0a30a2b872e593fe6d77
Simone Carletti <weppos@weppos.net>
2016-02-04 14:41:28 +0100

    Add real, live HTTP integration tests
    You can run them with the other tests by using
    
        $ DNSIMPLE_TOKEN=xyz go test ./... -v
    
    Or just run them
    
        $ DNSIMPLE_TOKEN=xyz go test ./... -v --run "TestLive"
    
    /cc @aeden

---
+++ dnsimple/live_test.go
@@ -0,0 +1,65 @@
+package dnsimple
+
+import (
+	"os"
+	"testing"
+	"fmt"
+)
+
+var (
+	dnsimpleLiveTest bool
+	dnsimpleToken    string
+	dnsimpleBaseURL  string
+	dnsimpleClient   *Client
+)
+
+func init() {
+	dnsimpleToken = os.Getenv("DNSIMPLE_TOKEN")
+	dnsimpleBaseURL = os.Getenv("DOMAINR_BASE_URL")
+
+	// Prevent peoeple from wiping out their entire production account by mistake
+	if dnsimpleBaseURL == "" {
+		dnsimpleBaseURL = "https://api.sandbox.dnsimple.com/"
+	}
+
+	if len(dnsimpleToken) > 0 {
+		dnsimpleLiveTest = true
+		dnsimpleClient = NewClient(NewOauthTokenCredentials(dnsimpleToken))
+		dnsimpleClient.BaseURL = dnsimpleBaseURL
+		dnsimpleClient.UserAgent = fmt.Sprintf("%v +livetest", dnsimpleClient.UserAgent)
+	}
+}
+
+func TestLive_Whoami(t *testing.T) {
+	if !dnsimpleLiveTest {
+		t.Skip("skipping live test")
+	}
+
+	whoami, _, err := dnsimpleClient.Auth.Whoami()
+	if err != nil {
+		t.Fatalf("Live whoami() returned error: %v", err)
+	}
+
+	fmt.Println(whoami.Account)
+	fmt.Println(whoami.User)
+}
+
+func TestLive_Domains(t *testing.T) {
+	if !dnsimpleLiveTest {
+		t.Skip("skipping live test")
+	}
+
+	whoami, _, err := dnsimpleClient.Auth.Whoami()
+	if err != nil {
+		t.Fatalf("Live whoami()/listDomains() returned error: %v", err)
+	}
+
+	accountID := whoami.Account.ID
+
+	domains, _, err := dnsimpleClient.Domains.List(fmt.Sprintf("%v", accountID))
+	if err != nil {
+		t.Fatalf("Live listDomains() returned error: %v", err)
+	}
+
+	fmt.Println(domains)
+}
