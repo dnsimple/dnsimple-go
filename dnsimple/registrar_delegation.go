21707bb655bd955a43a38b1fece7b61c9ff99c07
Luca Guidi <me@lucaguidi.com>
2016-07-11 12:39:14 +0200

    Added support for `changeDomainDelegationToVanity`

--- dnsimple/registrar_delegation.go
+++ dnsimple/registrar_delegation.go
@@ -7,10 +7,26 @@ import (
 // Domain represents a list of name servers that correspond to a domain delegation.
 type Delegation []string
 
+// Vanity Name Server
+type VanityNameServer struct {
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	IPv4      string `json:"ipv4,omitempty"`
+	IPv6      string `json:"ipv6,omitempty"`
+	CreatedAt string `json:"created_at,omitempty"`
+	UpdatedAt string `json:"updated_at,omitempty"`
+}
+
 // WhoisPrivacyResponse represents a response from an API method that returns a delegation struct.
 type DelegationResponse struct {
 	Response
 	Data *Delegation `json:"data"`
+}
+
+// Response for vanity name server enable and disable operations
+type VanityDelegationResponse struct {
+	Response
+	Data []VanityNameServer `json:"data"`
 }
 
 // GetDomainDelegation gets the current delegated name servers for the domain.
@@ -35,6 +51,22 @@ func (s *RegistrarService) GetDomainDelegation(accountID string, domainName stri
 func (s *RegistrarService) ChangeDomainDelegation(accountID string, domainName string, newDelegation *Delegation) (*DelegationResponse, error) {
 	path := versioned(fmt.Sprintf("/%v/registrar/domains/%v/delegation", accountID, domainName))
 	delegationResponse := &DelegationResponse{}
+
+	resp, err := s.client.put(path, newDelegation, delegationResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	delegationResponse.HttpResponse = resp
+	return delegationResponse, nil
+}
+
+// ChangeDomainDelegationToVanity enables vanity name servers for the given domain.
+//
+// See https://developer.dnsimple.com/v2/registrar/delegation/#delegateToVanity
+func (s *RegistrarService) ChangeDomainDelegationToVanity(accountID string, domainName string, newDelegation *Delegation) (*VanityDelegationResponse, error) {
+	path := versioned(fmt.Sprintf("/%v/registrar/domains/%v/delegation/vanity", accountID, domainName))
+	delegationResponse := &VanityDelegationResponse{}
 
 	resp, err := s.client.put(path, newDelegation, delegationResponse)
 	if err != nil {
